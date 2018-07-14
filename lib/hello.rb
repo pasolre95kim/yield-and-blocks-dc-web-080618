7eb765b0f7c73b31d3f6f37dfc4cc5987f89034f
Michelle Kim <pasolre95kim@gmail.com>
2018-07-14 20:55:11 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,7 @@
 
   def hello_t(array)
-    i = 0
-
-    while i < array.length
-      yield array[i]
-      i = i + 1
+    hello_t(["Tim", "Tom", "Jim"]) do |name|
+      if name.start_with?("T")
+        puts "Hi, #{name}"
+      end
     end
-  end
-  hello_t(["Tim", "Tom", "Jim"]) do |name|
-    if name.start_with?("T")
-      puts "Hi, #{name}"
-    end
-  end
