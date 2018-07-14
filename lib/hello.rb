11990b747e32fdd3006f16463ec3c5b5905b126e
Michelle Kim <pasolre95kim@gmail.com>
2018-07-14 21:01:13 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,6 @@
 def hello_t(array)
-  if block_given?
-    i = 0
-
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
-
-    array
-  else
-    puts "Hey! No block was given!"
+  hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
   end
 end
